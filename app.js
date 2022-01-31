c5d541b4cadcca7801909d192085959a3e033c6b
Adwait <adwait58@students.cs.ubc.ca>
2022-01-30 18:34:35 -0800

    Edited app.js

--- app.js
+++ app.js
@@ -64,6 +64,6 @@ app.post("/compose", (req, res) => {
   res.redirect("/");
 });
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || PORT, function () {
   console.log("Server started on port 3000");
 });
